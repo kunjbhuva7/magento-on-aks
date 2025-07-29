ca1f690dc5676616f35f04b23b5691086dd9dfbb
k-bhuva <kunjbhuva.me@gmail.com>
2025-07-29 13:40:32 +0530

    magento-89

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+  features = {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group
   location = var.location
@@ -23,3 +31,4 @@ resource "azurerm_kubernetes_cluster" "main" {
     env = "dev"
   }
 }
+
