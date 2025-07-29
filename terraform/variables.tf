cb7ecb0b376fa0239c4ee6f2533d9846a9128f6a
k-bhuva <kunjbhuva.me@gmail.com>
2025-07-29 15:08:49 +0530

    magento-2s

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,10 +3,10 @@ variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
 variable "resource_group" {
-  default = "magento-I"
+  default = "magento-C4"
 }
 variable "location" {
-  default = "East US"
+  default = "centralus"
 }
 variable "aks_name" {
   default = "magento-cluster"
