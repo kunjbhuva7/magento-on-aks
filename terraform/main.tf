649a1c0539b87fbe862ce4c04672170ecd013148
k-bhuva <kunjbhuva.me@gmail.com>
2025-07-29 16:19:22 +0530

    magento-7s

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_B1s"
+    vm_size    = "Standard_DS2_v2"
   }
 
   identity {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,10 +3,10 @@ variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
 variable "resource_group" {
-  default = "magento-2B"
+  default = "magento-4B"
 }
 variable "location" {
-  default = "centralus"
+  default = "West Europe"
 }
 variable "aks_name" {
   default = "magento-cluster-1"
